46e5f653c503da8b91ae69d09102131556271fc8
Bram Aristyo <BramAristyo@users.noreply.github.com>
2025-10-17 09:04:31 +0700

    feat(controller): add global response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,34 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function successResponse($data = [], $message = 'Success', $status = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+
+    protected function successResponseWithoutData($message = 'Success', $status = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+        ], $status);
+    }
+
+    protected function errorResponse($message = 'Error', $status = 400, $errors = [])
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+            'errors' => $errors,
+        ], $status);
+    }
+
+    protected function successRespond($data)
+    {
+        return response()->json($data);
+    }
 }
